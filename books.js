101799663d3d9aa91afee57bc8c4df411b2ae205
Bruno Sanches <brunopsanches@gmail.com>
2014-02-14 20:02:07 -0800

    Add Books mocked data - no DB yet

---
+++ books.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'The Lean Startup',
+    author: 'Eric Ries',
+    edition: 1,
+    pages: 300,
+    picture: ''
+  },
+
+  {
+    id: 2,
+    title: '1984',
+    author: 'George Orwell',
+    edition: 1,
+    pages: 500,
+    picture: ''
+  },
+
+  {
+    id: 3,
+    title: 'The Secrets of the JavaScript Ninja',
+    author: 'John Resig',
+    edition: 1,
+    pages: 400,
+    picture: ''
+  },
+
+  {
+    id: 4,
+    title: 'The Little Schemer',
+    author: 'Daniel P. Friedman and Matthias Felleisen',
+    edition: 4,
+    pages: 196,
+    picture: ''
+  }
+];
