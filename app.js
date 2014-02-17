aa6825fd70d0bae7e122b864e6179c27cdc16c85
Bruno Sanches <brunopsanches@gmail.com>
2014-02-17 03:39:58 -0800

    Use data from MongoDB to return Books instead of mocked data
    * Connect to mongodb using monk
    * Return everything from bookcollection on /books route

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var http = require('http'),
     send = require('send');
 
-var books = require('./books');
+var mongo = require('mongodb'),
+    monk = require('monk'),
+    db = monk('localhost:27017/bookshelf');
 
 var server = http.createServer(function(req, res) {
+  console.log(req.method)
+  console.log(req.url)
+
   switch (req.method) {
     case "GET":
       if (req.url === '/books') {
-        res.writeHead({ 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(books));
+        db.get('bookcollection').find({}, {}, function(e, docs) {
+          res.writeHead({ 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(docs));
+        });
       }
       else {
         send(req, req.url).root(__dirname).pipe(res);
